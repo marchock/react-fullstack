2f92bdba9e95cd5fbd5527d575306a36edbaed1b
ryan <ryanmarchock@gmail.com>
2017-08-20 22:24:02 +0100

    applied full url

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
+    callbackURL: 'https://salty-everglades-84856.herokuapp.com//auth/google/callback',
 }, (accessToken, refreshToken, profile, done) => {
 
 
