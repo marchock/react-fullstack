424850fb58250c63773c5b1dd22dbbdc29a2d457
ryan <ryanmarchock@gmail.com>
2017-08-20 15:57:55 +0100

    finished off flow

---
+++ config/prod.js
@@ -0,0 +1,13 @@
+// prod.js
+
+module.exports = {
+
+    googleClientID: proccess.env.GOOGLE_CLIENT_ID,
+
+    googleClientSecret: proccess.env.GOOGLE_CLIENT_SECRET,
+
+    mongoURI: proccess.env.MONGO_URI,
+
+    cookieKey: proccess.env.COOKIE_KEY,
+
+};
